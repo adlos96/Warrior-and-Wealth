2a305704412c915e391c6d860618c6a791d5159d
Adly <74104520+adlos96@users.noreply.github.com>
2026-01-10 17:23:41 +0100

    Update Server - Esperienza
    - Pulito codice calcolo esperienza

--- Server Strategico/Gioco/Esperienza.cs
+++ Server Strategico/Gioco/Esperienza.cs
@@ -1,80 +1,56 @@
-﻿namespace Server_Strategico.Gioco
+﻿using static Server_Strategico.Gioco.QuestManager;
+
+namespace Server_Strategico.Gioco
 {
     internal class Esperienza
     {
         public static int exp_Level_Up = 240;
         public static int cosa = 0;
-        public static double moltiplicatore = 0.62;
+        public static double moltiplicatore = 0.52;
 
-        public static async Task<int> LevelUp(Giocatori.Player player)
+        public static int LevelUp(Giocatori.Player player)
         {
-            Moltiplicatore(player);
             int esperienza = 0;
 
             switch (player.Livello)
             {
                 case 1:
-                    if (player.Esperienza >= exp_Level_Up + exp_Level_Up * player.Livello * 0.35)
-                    {
-                        player.Esperienza -= Convert.ToInt32(exp_Level_Up + exp_Level_Up * player.Livello * 0.35);
-                        player.Livello++;
-                    }
-                        esperienza = exp_Level_Up + Convert.ToInt32(exp_Level_Up * player.Livello * 0.35);
+                    moltiplicatore = 0.35;
                     break;
                 case 2:
-                    if (player.Esperienza >= exp_Level_Up + exp_Level_Up * player.Livello * 0.40)
-                    {
-                        player.Esperienza -= Convert.ToInt32(exp_Level_Up + exp_Level_Up * player.Livello * 0.40);
-                        player.Livello++;
-                    }
-                        esperienza = exp_Level_Up + Convert.ToInt32(exp_Level_Up * player.Livello * 0.40);
+                    moltiplicatore = 0.40;
                     break;
                 case 3:
-                    if (player.Esperienza >= exp_Level_Up + exp_Level_Up * player.Livello * 0.45)
-                    {
-                        player.Esperienza -= Convert.ToInt32(exp_Level_Up + exp_Level_Up * player.Livello * 0.45);
-                        player.Livello++;
-                    }
-                        esperienza = exp_Level_Up + Convert.ToInt32(exp_Level_Up * player.Livello * 0.45);
+                    moltiplicatore = 0.45;
                     break;
                 default:
-                    if (player.Esperienza >= exp_Level_Up + exp_Level_Up * player.Livello * moltiplicatore)
-                    {
-                        player.Esperienza -= Convert.ToInt32(exp_Level_Up + exp_Level_Up * player.Livello * moltiplicatore);
-                        player.Livello++;
-                        if (player.Livello >= 10 && player.Livello < 11) cosa = 1;
-                        else if (player.Livello >= 20 && player.Livello < 50) cosa = 2;
-                        else if (player.Livello >= 50 && player.Livello < 80) cosa = 3;
-                        else if (player.Livello >= 80 && player.Livello < 110) cosa = 4;
-                        else if (player.Livello >= 110) moltiplicatore = 0.98;
-                    }
-                        esperienza = exp_Level_Up + Convert.ToInt32(exp_Level_Up * player.Livello * moltiplicatore);
+                    if (player.Livello >= 10 && player.Livello < 20) cosa = 1;
+                    else if (player.Livello >= 20 && player.Livello < 50) cosa = 2;
+                    else if (player.Livello >= 50 && player.Livello < 80) cosa = 3;
+                    else if (player.Livello >= 80 && player.Livello < 110) cosa = 4;
+                    else if (player.Livello >= 110) moltiplicatore = 0.98;
+
+                    Moltiplicatore(player);
                     break;
             }
+            esperienza = exp_Level_Up + (int)(exp_Level_Up * player.Livello * moltiplicatore);
+            if (player.Esperienza >= esperienza)
+            {
+                player.Esperienza -= esperienza;
+                OnEvent(player, QuestEventType.Livelli, "non serve", 1);
+                player.Livello++;
+                if (Server.Server.Client_Connessi.Contains(player.guid_Player) && Server.Server.Client_Connessi.Count() == 0) Server.ServerConnection.DescUpdate(player);
+            }
+            if (player.Livello > player.QuestProgress.CurrentProgress[59]) OnEvent(player, QuestEventType.Livelli, "non serve", 1);
             return esperienza;
         }
         public static void Moltiplicatore(Giocatori.Player player)
         {
-            if (player.Livello >= 10 && player.Livello < 20 && cosa == 1)
-            {
-                moltiplicatore = 0.61;
-                cosa = 0;
-            }
-            else if (player.Livello >= 20 && player.Livello < 40 && cosa == 2)
-            {
-                moltiplicatore = 0.67;
-                cosa = 0;
-            }
-            else if (player.Livello >= 40 && cosa == 3)
-            {
-                moltiplicatore = 0.74;
-                cosa = 0;
-            }
-            else if (player.Livello >= 40 && cosa == 4)
-            {
-                moltiplicatore = 0.81;
-                cosa = 0;
-            }
+            if (cosa == 1) moltiplicatore = 0.58;
+            else if (cosa == 2) moltiplicatore = 0.64;
+            else if (cosa == 3) moltiplicatore = 0.71;
+            else if (cosa == 4) moltiplicatore = 0.79;
+            cosa = 0;
         }
     }
 }
